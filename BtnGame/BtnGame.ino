382c53f7e1e90e353b30318a7b0fc831c832cd4d
rogino <rio.ogino@gmail.com>
2017-06-06 21:40:01 +1200

    Commented Code

--- BtnGame/BtnGame.ino
+++ BtnGame/BtnGame.ino
@@ -5,86 +5,95 @@
 const int btnPin = 12;
 //Button connected to ground, 5v and 4k7 resistor. LCD connected to 20, 21 (SCL, SDA), 5v and gnd
 
+unsigned long startTime = 0; //The time in which the measurement starts
+unsigned long bestTime = ULONG_MAX; //The shortest time. initialized to be biggest so that anything (should) be smaller
+char str[16]; //Enough characters to fit on one line
+
 
 LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address
-void setup() {
-  pinMode(btnPin, INPUT);   
-  Serial.begin(9600);  // Used to type in characters
-  lcd.begin(16,2);   // initialize the lcd for 16 chars 2 lines, turn on backlight
-  lcd.backlight();
-  lcd.print("Click to begin");
-  while (digitalRead(btnPin) == LOW) {
-    delay(1);
-  }
-
-}
-unsigned long startTime = 0;
-unsigned long bestTime = ULONG_MAX; //The best time. initialized to be biggest
-char str[16];
 
 unsigned long waitForBtn() {
   if (digitalRead(btnPin) == HIGH) {
-    //Stop people from pressing the whole time
+    //Stop people from pressing from the start
     lcd.clear();
     lcd.print("Too early");
-    delay(500);
-    return 0;
+    delay(500); //Show text for 500 milliseconds
+    return 0; //Return 0
   }
-  startTime = millis(); //start time
+
   while (digitalRead(btnPin) == LOW) {
     delay(1); //read every millisecond
   }
   unsigned long value = millis() - startTime;
+  //Find difference
   lcd.clear();
   sprintf(str, "Time: %lums", value);
   lcd.print(str);
+  //sprintf to have variable
   if (value < bestTime) {
-    bestTime = value; //change value of bestTime to new bestTime
+    //Change best time if required
+    bestTime = value;
   }
-  return value; //return difference
+  return value;
+}
+
+
+
+void setup() {
+  pinMode(btnPin, INPUT); //pin mode
+  Serial.begin(9600); //Used to type in characters
+  lcd.begin(16,2); //initialize the lcd for 16 chars 2 lines, turn on backlight
+  lcd.backlight();
+  lcd.print("Click to begin"); //Initial text
+  while (digitalRead(btnPin) == LOW) {
+    //Keep inside setup and show text until button is pressed
+    delay(1);
+  }
 }
 
 
 void loop() {
-  // put your main code here, to run repeatedly:
   lcd.clear();
   lcd.setCursor(0,0);
   lcd.print("Three");
   delay(1000);
   lcd.setCursor(0,0);
-  lcd.print("Two  ");
+  lcd.print("Two  "); //Spaces so there is no need for a lcd.clear()
   delay(1000);
   lcd.setCursor(0,0);
   lcd.print("One   ");
   lcd.setCursor(0,0);
-  delay(random(50, 3000));
+  delay(random(50, 3000)); //Between 0.05 and 3 seconds
   lcd.print("BANG!");
+  startTime = millis(); //start the time. Here instead of in function to increase accuracy
   unsigned long result = waitForBtn();
+  //Get time
 
-  
-  delay(200);
-  int counter = 0;
+  delay(200); //wait 0.2s. Removing this is really bad for some reason
 
-  int len = 700;
+  int counter = 0; //Counter for while(true) loop
+  int len = 700; //Length of time for text to be flashing between two states
+  lcd.setCursor(0,1); //Set to go to second line/row
   while (digitalRead(btnPin) == LOW) {
-    lcd.setCursor(0,1);
+    //Once button is pressed, restart
     if (counter % len == 0) {
+      //If counter is either 0 or len, it will update text. In these cases, clear the line
       lcd.print("                ");//clear second line: 16 spaces
-      lcd.setCursor(0,1); //Second line
-      Serial.print("HERE");
-
+      lcd.setCursor(0,1); //Move cursor to second line
     }
+
     if (counter == len) {
-      //update every second
+      //Second line switches between two values. Update when equals
       lcd.print("Click to restart");
     }
     else if (counter == 0) {
+      //Update counter when modulus is 0
       sprintf(str, "Best: %lums", bestTime);
-      lcd.print(str);
+      lcd.print(str); //Best time
     }
+    
     //update counter
     counter = (counter + 1)%(len * 2);
-   
     delay(1);
   }
 }
