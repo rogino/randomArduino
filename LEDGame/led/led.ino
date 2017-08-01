26a69961926c3f9fccabdd87e1c98c1f9b2c0633
rogino <rio.ogino@gmail.com>
2017-08-01 15:56:11 +1200

    more work on LED

--- LEDGame/led/led.ino
+++ LEDGame/led/led.ino
@@ -1,11 +1,12 @@
 #include <Servo.h>
-const int led[] = {2, 4, 5, 7};
-const int ldr = 0;
-int active = 0; //Initialize at 0
+const int led[] = {3, 4, 5, 6};
+const int ledPos[] = {60, 80, 100, 120};
+const int ldr = 5;
+int active = -1; //Initialize at 0
 const int arrLen = sizeof(led)/sizeof(int);
-const int servo1Pin = 8;
+const int servo1Pin = 2;
+const int motorPin = 10;
 Servo servo1;
-
 void setup() {
   Serial.begin(9600);
   // put your setup code here, to run once:
@@ -16,17 +17,114 @@ void setup() {
   pinMode(servo1Pin, OUTPUT);
   servo1.attach(servo1Pin);
   //servo1.write(130);
-  randomSeed(15);
+  randomSeed(1);
+  pinMode(motorPin, OUTPUT);
+
+//  for (int i = 0; i < 1023; i++) {
+//    Serial.println(map(i,200,1000,60,120));
+//    delay(10);
+//  }
 }
 
-int rotation = 0;
-bool fowards = true;
-void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println(analogRead(ldr));
-  digitalWrite(led[active], LOW); //Turn off LED
-  active = random(0, arrLen);
-  digitalWrite(led[active], HIGH);
-  servo1.write(map(analogRead(ldr), 700, 800, 0, 180));
-  delay(random(50, 250));
+int selectedLed(int position, int variance) {
+  for (int i = 0; i < arrLen; i++) {
+    if (abs(ledPos[i] - position) < variance) {
+      return i;
+    }
+  }
+  return -1; //No match, return -1
 }
+
+void motorCountdown(int num) {
+  for (int i = 0; i < num; i++) {
+   digitalWrite(motorPin,HIGH);
+    delay(300);
+    digitalWrite(motorPin, LOW);
+    if (i < num - 1) {
+      delay(700);
+    }
+  }
+}
+
+void flashLed(int time, int number) {
+  analogWrite(motorPin, 100);
+  for (int i = 0; i < number; i++) {
+    for (int j = 0; j < arrLen; j++) {
+      digitalWrite(led[j], HIGH);
+    }
+    delay(time);
+    for (int j = 0; j < arrLen; j++) {
+      digitalWrite(led[j], LOW);
+    }
+    delay(time);
+  }
+  analogWrite(motorPin, 0);
+}
+
+
+int matches = 0;
+int requiredMatches = 8;
+int target = 0;
+unsigned long time = millis();
+int originalSpeed = 10000;
+int speed = originalSpeed;
+int speedIncrement = 50;
+
+int minLdr = 480;
+int maxLdr = 580;
+
+
+void loop() {
+  //motorCountdown(3);
+  digitalWrite(led[target], LOW);
+  target = random(0, arrLen);
+  digitalWrite(led[target], HIGH);
+  time = millis();
+  Serial.println(time);
+  Serial.println("Started");
+  Serial.println(millis()-time);
+  while (millis() - time < speed) {
+    Serial.println(millis()-time);
+    delay(50);
+    int ldrVal = constrain(analogRead(ldr), minLdr, maxLdr);
+    int pos = map(ldrVal, maxLdr, minLdr, 50, 130);
+
+    servo1.write(pos);
+    active = selectedLed(pos, 10);
+    /*Serial.println(active);
+    Serial.println(target);*/
+    if (active == target) {
+      matches++;
+      /*
+      Serial.println("Match");
+      Serial.println(matches);
+      Serial.println(requiredMatches);*/
+    }
+    else {
+      matches = 0;
+      /*
+      Serial.println("not match");
+      Serial.println(matches);*/
+    }
+    
+    if (matches >= requiredMatches) {
+      //If more matches than rquired, exit loop
+      break;
+    }
+  }
+ 
+  if (matches < requiredMatches) {
+     //If exited because faliure
+    //Set back to original speed
+    speed = originalSpeed;
+    flashLed(100, 5);
+    delay(1000);
+  }
+  
+  else {
+    speed -= speedIncrement;
+    //decrease speed: increase difficulty
+  }
+  Serial.println("exited");
+}
+
